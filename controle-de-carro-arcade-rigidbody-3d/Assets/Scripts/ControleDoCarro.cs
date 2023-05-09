2ea6a161ba17a6cb3bdc1b4f00c2d589fd4fa91b
GabrielChiarelli <102618272+GabrielChiarelli@users.noreply.github.com>
2023-05-09 15:39:57 -0300

    Parte 6.

--- controle-de-carro-arcade-rigidbody-3d/Assets/Scripts/ControleDoCarro.cs
+++ controle-de-carro-arcade-rigidbody-3d/Assets/Scripts/ControleDoCarro.cs
@@ -38,6 +38,15 @@ public class ControleDoCarro : MonoBehaviour
     [SerializeField] private Transform rodaFrontalEsquerda;
     [SerializeField] private Transform rodaFrontalDiretia;
 
+    [Header("Inclinação do Carro na Rampa")]
+    [SerializeField] private float velocidadeDeInclinacaoNaRampa;
+    [SerializeField] private float tamanhoDosRaiosDeVerificacao;
+    [SerializeField] private Transform localDoRaioDeVerificacao01;
+    [SerializeField] private Transform localDoRaioDeVerificacao02;
+    [SerializeField] private LayerMask layerDoChao;
+    private RaycastHit localAtingidoPeloRaio;
+    private bool estaNoChao;
+
     private void Awake()
     {
         oRigidbody = GetComponent<Rigidbody>();
@@ -55,6 +64,7 @@ public class ControleDoCarro : MonoBehaviour
     {
         ReceberInputs();
         RotacionarRodas();
+        InclinarNaRampa();
         RotacionarCarro();
     }
 
@@ -85,6 +95,43 @@ public class ControleDoCarro : MonoBehaviour
 
         // Rotaciona a Roda da Direita
         rodaFrontalDiretia.localRotation = Quaternion.Euler(rodaFrontalDiretia.localRotation.eulerAngles.x, (inputDeRotacao * anguloMaximoDeRotacaoDasRodas), rodaFrontalDiretia.localRotation.eulerAngles.z);
+    }
+
+    private void InclinarNaRampa()
+    {
+        Vector3 normalDoPiso = Vector3.zero;
+
+        if (Physics.Raycast(localDoRaioDeVerificacao01.position, -transform.up, out localAtingidoPeloRaio, tamanhoDosRaiosDeVerificacao, layerDoChao))
+        {
+            estaNoChao = true;
+            normalDoPiso = localAtingidoPeloRaio.normal;
+        }
+        else
+        {
+            estaNoChao = false;
+            normalDoPiso = Vector3.zero;
+        }
+
+        if (Physics.Raycast(localDoRaioDeVerificacao02.position, -transform.up, out localAtingidoPeloRaio, tamanhoDosRaiosDeVerificacao, layerDoChao))
+        {
+            estaNoChao = true;
+            normalDoPiso = (normalDoPiso + localAtingidoPeloRaio.normal) / 2f;
+        }
+
+        //  Reseta a inclinação no ar
+        /*
+        if (estaNoChao)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.up, normalDoPiso) * transform.rotation, velocidadeDeInclinacaoNaRampa * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.up, normalDoPiso), velocidadeDeInclinacaoNaRampa * Time.deltaTime);
+        }
+        */
+
+        // Deixa o carro inclinado no ar
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.up, normalDoPiso) * transform.rotation, velocidadeDeInclinacaoNaRampa * Time.deltaTime);
     }
 
     private void MoverCarro()
